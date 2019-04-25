aa96d024ae6ce70479b607611a1bdae9e92eaef9
Daniel Shroff <danielshroff@hotmail.com>
2019-04-25 16:32:47 +0530

    Split files

---
+++ GeoSend/DataTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+namespace GeoSend.Lan
+{
+    public class DataTypes
+    {
+        public struct Node
+        {
+            public bool forward;
+            public IPAddress nodeIP;
+            public string nodeName;
+            public int nodeSerial;
+        }
+    }
+}
